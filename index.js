41bced15d5cc0bf9e337acd88abc88c02362385a
Sarah <sarahehi.sa@gmail.com>
2020-04-23 17:58:53 +0100

    Sock Merchant solution

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-
-
 function sortAndCount( n, arr ) {
     let sorted = arr.sort( (a,b) => a - b);
     let pairs = 0;
-    //complete the function block
+    for (let i = 0; i < n - 1; i++) {
+        if ( sorted[i] === sorted[i + 1]) {
+            pairs++;
+            i += 1;
+        }
+    }
     console.log(pairs)
     return pairs;
 }
